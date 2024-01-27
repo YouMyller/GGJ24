341e09669d39969cc2a139aa4261465e71ae16c6
juumyllyla <31961349+juumyllyla@users.noreply.github.com>
2024-01-27 15:43:29 +0200

    Elevator button press

---
+++ Assets/ElevatorButton.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorButton : MonoBehaviour
+{
+    private bool onButton;
+
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    private float minDistanceToPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Vector3.Distance(player.transform.position, transform.position) < minDistanceToPlayer)
+        {
+            print("Button pressed");
+        }
+
+    }
+
+    void OnMouseOver()
+    {
+        onButton = true;
+    }
+
+    void OnMouseExit()
+    {
+        onButton = false;
+    }
+}
